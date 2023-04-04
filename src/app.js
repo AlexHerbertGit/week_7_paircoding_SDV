d8e655ad73f39ba129e2d57d218347cacc084fd7
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-04-05 11:27:28 +1200

    array of arrays fucntion

--- src/app.js
+++ src/app.js
@@ -3,3 +3,17 @@ function getNumbers(arr) {
 }
 console.log(getNumbers([1, 2, "alex",  3, 4, "herbert"]))
 
+function arrayOfArrays(arr) {
+    let newArray = [];
+    for(let i = 0; i < arr.length; i++) {
+        let largest = arr[i][0]
+        for(let j = 0; j < arr[i].length; j++){
+            if(arr[i][j] > largest){
+                largest = arr[i][j];
+            }
+        }
+        newArray.push(largest)
+    }
+    return newArray
+}
+console.log(arrayOfArrays([1, 2, 3,], [4, 5, 6,], [10, 20, 100]))
